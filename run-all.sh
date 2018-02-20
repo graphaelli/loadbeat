3436f585c0d314447a219ab0790a33851e05c547
Gil Raphaelli <g@raphaelli.com>
2018-02-20 15:26:18 -0500

    shorter runs, longer rests

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-for profile in profiles/*; do
+for profile in profiles/*mb.yml; do
     docker restart localtesting_6.3.0-SNAPSHOT_apm-server
-    sleep 30
+    sleep 60
 
     NOW=$(date '+%Y-%m-%d %H:%M:%S')
-    ./loadbeat -e -E loadbeat.base_urls=["http://localhost:8200/"] -E 'output.elasticsearch.hosts=["localhost:9200"]' -E loadbeat.run_timeout=10m -c $profile &> ${profile}.log
+    ./loadbeat -e -E loadbeat.base_urls=["http://localhost:8200/"] -E 'output.elasticsearch.hosts=["localhost:9200"]' -E loadbeat.run_timeout=5m -c $profile &> ${profile}.log
     echo ${profile} ${NOW} - $(date '+%Y-%m-%d %H:%M:%S')
 done
 
